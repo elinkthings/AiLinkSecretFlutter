46d648574d3d74585c74d017970ff7fbc27f7b50
suzhi <suzhi@elinkthings.com>
2023-12-29 22:02:51 +0800

    feat: Flutter调用Android decryptBroadcast验证

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,6 +4,12 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:ailink/ailink.dart';
 
+extension Uint8ListExtension on Uint8List {
+  String toHex() {
+    return map((byte) => '0x${(byte.toRadixString(16).padLeft(2, '0')).toUpperCase()}').join(',');
+  }
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -18,6 +24,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
   final _ailinkPlugin = Ailink();
+  Uint8List? _decryptedPayload;
+
 
   @override
   void initState() {
@@ -28,11 +36,13 @@ class _MyAppState extends State<MyApp> {
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
+    Uint8List? decryptedPayload;
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await _ailinkPlugin.getPlatformVersion() ?? 'Unknown platform version';
+      platformVersion = await _ailinkPlugin.getPlatformVersion() ?? 'Unknown platform version';
+      final payload = [0x01, 0x1c, 0x01, 0x2c, 0x12, 0x01, 0x01, 0xba, 0x01, 0x42, 0xc6, 0x93, 0xe5, 0x6d, 0xa2, 0xd0, 0x22, 0x05, 0xff, 0xff];
+      decryptedPayload = await _ailinkPlugin.decryptBroadcast(Uint8List.fromList(payload));
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
@@ -45,6 +55,10 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _platformVersion = platformVersion;
     });
+
+    setState(() {
+      _decryptedPayload = decryptedPayload;
+    });
   }
 
   @override
@@ -54,8 +68,12 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Running on: $_platformVersion\n'),
+            Text('Decrypted Payload: ${_decryptedPayload?.toHex()}\n'),
+          ],
         ),
       ),
     );
